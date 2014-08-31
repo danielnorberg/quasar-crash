775b81f6b2642612f5a718d8c0e750c4cca2667e
Daniel Norberg <dano@spotify.com>
2014-08-31 16:06:01 +0200

    different, real, issue

--- src/main/java/QuasarCrashRepro.java
+++ src/main/java/QuasarCrashRepro.java
@@ -1,35 +1,24 @@
+import com.google.common.base.Throwables;
+
+import java.util.concurrent.ExecutionException;
+import java.util.stream.IntStream;
+
 import co.paralleluniverse.fibers.Fiber;
-import co.paralleluniverse.fibers.SuspendExecution;
 import co.paralleluniverse.strands.SuspendableRunnable;
-import co.paralleluniverse.strands.channels.Channel;
-import co.paralleluniverse.strands.channels.Channels;
 
 public class QuasarCrashRepro {
 
-  public static void main(final String... args) throws InterruptedException, SuspendExecution {
-    final int M = 10;
-
-    final Channel<String> requests = Channels.newChannel(M * 2);
-    final Channel<String> replies = Channels.newChannel(M * 2);
-
-    // Put M requests in-flight
-    for (int i = 0; i < M; i++) {
-      requests.send("ping");
-      new Fiber<>((SuspendableRunnable) () -> {
-        requests.receive();
-        replies.send("pong");
-      }).start();
-    }
-
-    // Spawn a new request for every reply
-    while (true) {
-      replies.receive();
-      requests.send("ping");
-      new Fiber<>((SuspendableRunnable) () -> {
-        requests.receive();
-        replies.send("pong");
-      }).start();
-    }
+  public static void main(final String... args) {
+    new Fiber<>((SuspendableRunnable) () -> {
+      IntStream.range(0, 1)
+          .mapToObj((s) -> new Fiber<>(() -> "pong").start())
+          .map((t) -> {
+            try {
+              return t.get();
+            } catch (ExecutionException | InterruptedException e) {
+              throw Throwables.propagate(e);
+            }
+          });
+    }).start();
   }
-
 }
